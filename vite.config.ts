f8a6fef0b6359bdb8f09101befe53ecf90fdbdc4
minsaadun <minsaadun@gmail.com>
2026-07-21 01:55:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-     base: '/pishing-awareness-simulator/',
+     base: '/phishing-awareness-simulator/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
